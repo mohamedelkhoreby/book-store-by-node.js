f48843fec07216182d998b8308ae51184a0786f5
mohamedelkhoreby <50024109+mohamedelkhoreby@users.noreply.github.com>
2023-07-20 12:56:33 +0300

    update

--- book/routes/users.js
+++ book/routes/users.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/api/books', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/api/books/:id', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/api/books/post', function(req, res, next) {
+  res.send('respond with a resource');
+});
 module.exports = router;
